c4bfee5ce3028193552ca860547e4665b4d1947a
guanzhibin <37924946+guanzhibin@users.noreply.github.com>
2019-01-14 10:40:28 +0800

    Update app_setting.py

--- mg_deal_data/public_class/app_setting.py
+++ mg_deal_data/public_class/app_setting.py
@@ -1,5 +1,5 @@
 #coding:utf8
 email_host="smtp.sina.com"  #设置服务器
-email_user="xg805115189@sina.com"    #用户名
-email_pass="Guan891212"   #口令 
+email_user="xg8051xx189@sina.com"    #用户名
+email_pass="xxxxx891212"   #口令 
 subtype = 'html' #发送类型
